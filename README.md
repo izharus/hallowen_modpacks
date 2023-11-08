4ed6af29227d56f94bacec1b8cde0fbdf0cb11ea
izharus <ruslan.izhakovskij@gmail.com>
2023-11-08 04:31:11 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,91 @@
+# Minecraft Mod Pack: Server and Client Customizations
+
+## Table of Contents
+- [Overview](#overview)
+- [Getting Started](#getting-started)
+  - [Prerequisites](#prerequisites)
+  - [Installation](#installation)
+- [Contents](#contents)
+- [JSON File Format](#json-file-format)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Overview
+This Minecraft mod pack is designed to enhance your gaming experience by offering a range of customizations for both server and client environments. It provides flexibility for various data types, allowing you to tailor your Minecraft world to your preferences.
+
+## Multiple Server Support
+
+The `map.json` file supports multiple servers. In the modpack directory, you can find separate server folders, such as "terrafirmacraft" and "terrafirmacraft_test" While there may be only one modpack built at the moment, this logic is designed to support multiple modpacks in the future.
+
+If you plan to create additional modpacks, you can organize them in their respective server folders and configure the `map.json` files accordingly. This flexibility allows you to manage and distribute various Minecraft modpacks with ease.
+
+## Getting Started
+Follow these steps to get started with the mod pack.
+
+### Prerequisites
+- Python 3.11
+- Windows 10 (tested platform)
+
+### Installation
+1. Clone this repository to your local machine.
+2. Set the base URL of your mod repository in the `json_maker_hook.py` file. Example: `"https://raw.githubusercontent.com/izharus/tfc_halloween_modpack/"`.
+3. Install the required dependencies by running `pip install -r requirements/dev.txt`.
+4. Set up pre-commit hooks to automate the creation and updating of `map.json`. These hooks include linting tools and the `json_maker_hook`, which automatically generates or updates `map.json` when you add new files.
+5. Add new files to the repository and commit your changes. The pre-commit hook will take care of updating `map.json`.
+
+## Contents
+The mod pack is organized into the following categories:
+
+- **main_data**
+  - Compatibility: Server and Client
+  - Description: Essential data files for both server and client to ensure compatibility and optimal performance.
+
+- **server_data**
+  - Compatibility: Server
+  - Description: Server-specific data that enhances gameplay and features on the server side. Install these on your server for an improved gaming experience.
+
+- **client_data**
+  - Compatibility: Client
+  - Description: Data files intended for clients, providing additional customizations to enhance your personal gameplay. You have the option to install both required and additional data as per your preferences.
+
+- **client_additional_data**
+  - Compatibility: Client
+  - Description: Additional files that can be added to the client data if needed. The launcher may add some fields to the client data before installation.
+
+## JSON File Format
+Each file in the directory should follow a specific JSON format (file_data_dict):
+
+```json
+{
+  "file_name": "file_name",
+  "api_url": "mods/file_name", # used for downloading
+  "hash": "hash_value", # hash of the file
+  "install_on_client": false, # true if it needs to be installed on the client
+  "install_on_server": true, # true if it needs to be installed on the server
+  "dist_file_path": "where_to_download_file"
+}
+```
+## Types of Keys in `map.json`
+
+There are four types of keys in the `map.json` file:
+
+- **main_data**: A list of files that should have fields "install_on_client" and "install_on_server" set to True.
+
+- **client_data**: A list of files where "install_on_client" is True and "install_on_server" is False.
+
+- **server_data**: A list of files that are the same as `client_data`, but intended only for server installation.
+
+- **client_additional_data**: A list of numerous additional files for clients, similar to `client_data`. The launcher may add some fields to `client_data` before installing.
+
+## Usage
+
+This mod pack is ready to enhance your Minecraft experience. Make sure to install the required data on the server and client sides, and customize your game as you see fit.
+
+## Contributing
+
+Feel free to contribute to this mod pack by adding new files, improving documentation, or suggesting enhancements. Create a pull request to share your contributions.
+
+## License
+
+This mod pack is released under the License Name license. See the LICENSE.md file for details.
---
+++ src/test/test_json_maker_hook.py
@@ -0,0 +1,261 @@
+"""Tests for src/json_maker_hook.py"""
+# pylint:disable = E0401, R0123
+import hashlib
+import os
+
+import pytest
+from src.json_maker_hook import (
+    CalculateHashFailed,
+    calculate_hash,
+    generate_file_info,
+    generate_json,
+)
+
+
+def test_incorrect_hash_value_from_calculate_hash(tmpdir):
+    """Test if calculate_hash() calculates incorrect hashes"""
+    file_content = b"Hello, World!"
+    file_path = tmpdir.join("test_file.txt")
+    file_path.write(file_content)
+
+    hash_value = calculate_hash(file_path, "sha256")
+    expected_hash = hashlib.sha256(file_content).hexdigest()
+
+    assert hash_value == expected_hash
+
+
+def test_calculate_hash_not_raises_error(tmpdir):
+    """Test if calculate_hash() handles non-existent files."""
+    non_existent_file = tmpdir.join("non_existent_file.txt")
+
+    with pytest.raises(CalculateHashFailed):
+        calculate_hash(non_existent_file, "sha256")
+
+
+def test_calculate_hash_uses_invalid_algorithm(tmpdir):
+    """Test if calculate_hash() handles invalid hash algorithms."""
+    file_content = b"Hello, World!"
+    file_path = tmpdir.join("test_file.txt")
+    file_path.write(file_content)
+
+    with pytest.raises(CalculateHashFailed):
+        calculate_hash(file_path, "invalid_algorithm")
+
+
+def test_generate_file_generates_invalid_data(tmpdir):
+    """Test the generate_file_info() function."""
+    # Create a temporary directory for testing
+    root_directory = tmpdir.mkdir("test_modpacks")
+    base_api_url = "https://example.com/api/"
+    is_install_on_client = True
+    is_install_on_server = False
+
+    # Create some test files in the temporary directory
+    file1 = root_directory.join("file1.txt")
+    file1.write("File 1 content")
+
+    file2 = root_directory.mkdir("subdirectory").join("file2.txt")
+    file2.write("File 2 content")
+
+    # Generate file information
+    file_info = generate_file_info(
+        root_directory,
+        base_api_url,
+        is_install_on_client,
+        is_install_on_server,
+    )
+
+    # Check the generated file information
+    assert len(file_info) == 2
+
+    # File 1
+    assert file_info[0]["file_name"] == "file1.txt"
+    assert file_info[0]["api_url"] == "https://example.com/api/file1.txt"
+    assert file_info[0]["install_on_client"] is True
+    assert file_info[0]["install_on_server"] is False
+    assert "hash" in file_info[0]
+    assert "dist_file_path" in file_info[0]
+
+    # File 2
+    assert file_info[1]["file_name"] == "file2.txt"
+    assert (
+        file_info[1]["api_url"]
+        == "https://example.com/api/subdirectory/file2.txt"
+    )
+    assert file_info[1]["install_on_client"] is True
+    assert file_info[1]["install_on_server"] is False
+    assert "hash" in file_info[1]
+    assert "dist_file_path" in file_info[1]
+
+    # Add more test cases as needed
+
+
+def test_generate_file_info_should_return_empty_list_for_empty_dir(tmpdir):
+    """Test generate_file_info() with an empty directory."""
+    # Create an empty directory for testing
+    root_directory = tmpdir.mkdir("empty_modpacks")
+    base_api_url = "https://example.com/api/"
+    is_install_on_client = True
+    is_install_on_server = False
+
+    # Generate file information for an empty directory
+    file_info = generate_file_info(
+        root_directory,
+        base_api_url,
+        is_install_on_client,
+        is_install_on_server,
+    )
+
+    # The result should be an empty list
+    assert file_info is []
+
+
+def test_generate_file_return_invalid_data_for_nested_files(tmpdir):
+    """Test generate_file_info() with subdirectories."""
+    # Create a directory structure for testing
+    root_directory = tmpdir.mkdir("modpacks")
+    root_directory.join("file1.txt").write("File 1 content")
+    subdirectory = root_directory.mkdir("subdirectory")
+    subdirectory.join("file2.txt").write("File 2 content")
+
+    base_api_url = "https://example.com/api/"
+    is_install_on_client = True
+    is_install_on_server = False
+
+    # Generate file information
+    file_info = generate_file_info(
+        root_directory,
+        base_api_url,
+        is_install_on_client,
+        is_install_on_server,
+    )
+
+    # Check the generated file information
+    assert len(file_info) == 2
+
+    # Check the file information for file1.txt
+    assert file_info[0]["file_name"] == "file1.txt"
+    assert file_info[0]["dist_file_path"] == "file1.txt"
+    assert file_info[0]["api_url"] == "https://example.com/api/file1.txt"
+    assert file_info[0]["install_on_client"] is True
+    assert file_info[0]["install_on_server"] is False
+    assert "hash" in file_info[0]
+    assert "dist_file_path" in file_info[0]
+
+    # Check the file information for file2.txt in the subdirectory
+    assert file_info[1]["file_name"] == "file2.txt"
+    assert file_info[1]["dist_file_path"] == "subdirectory\\file2.txt"
+    assert (
+        file_info[1]["api_url"]
+        == "https://example.com/api/subdirectory/file2.txt"
+    )
+    assert file_info[1]["install_on_client"] is True
+    assert file_info[1]["install_on_server"] is False
+    assert "hash" in file_info[1]
+    assert "dist_file_path" in file_info[1]
+
+    # Add more test cases as needed
+
+
+def test_generate_json_missing_fields(tmpdir):
+    """Test the generate_json() function."""
+    # Create a temporary directory for testing
+    modpacks_directory = tmpdir.mkdir("modpacks")
+    base_api_url = "https://example.com/api/"
+
+    # Create subdirectories and files within the "modpacks" directory
+    modpack1 = modpacks_directory.mkdir("modpack1")
+    modpack1.mkdir("main_data").join("file1.txt").write("File 1 content")
+    modpack1.mkdir("client_data").join("file2.txt").write("File 2 content")
+    modpack1.mkdir("server_data").join("file3.txt").write("File 3 content")
+
+    modpack2 = modpacks_directory.mkdir("modpack2")
+    modpack2.mkdir("main_data").join("file4.txt").write("File 4 content")
+    modpack2.mkdir("client_data").join("file5.txt").write("File 5 content")
+    modpack2.mkdir("server_data").join("file6.txt").write("File 6 content")
+
+    # Execute the generate_json function with the specified JSON file path
+    map_json = generate_json(modpacks_directory, base_api_url)
+    assert map_json
+
+    assert len(map_json) == 2  # Two modpacks
+
+    # Verify modpack1 information
+    assert "modpack1" in map_json
+    assert "main_data" in map_json["modpack1"]
+    assert "client_data" in map_json["modpack1"]
+    assert "server_data" in map_json["modpack1"]
+
+    # Verify modpack2 information
+    assert "modpack2" in map_json
+    assert "main_data" in map_json["modpack2"]
+    assert "client_data" in map_json["modpack2"]
+    assert "server_data" in map_json["modpack2"]
+
+    # You can add more assertions to check the contents of the JSON file
+
+
+def test_generate_json_returns_non_empty_dato_for_empty_dir(tmpdir):
+    """Test generate_json() with an empty directory."""
+    # Create an empty "modpacks" directory for testing
+    modpacks_directory = tmpdir.mkdir("modpacks")
+    base_api_url = "https://example.com/api/"
+
+    # Execute the generate_json function with an empty directory
+    map_json = generate_json(str(modpacks_directory), base_api_url)
+
+    assert map_json is {}
+
+
+def test_generate_json_return_nonempry_dict_for_nonexistent_directory(tmpdir):
+    """Test generate_json() with a non-existent directory."""
+    # Specify a non-existent directory for testing
+    non_existent_directory = os.path.join(str(tmpdir), "non_existent_modpacks")
+    base_api_url = "https://example.com/api/"
+
+    # Execute the generate_json function with a non-existent directory
+    map_json = generate_json(non_existent_directory, base_api_url)
+
+    # Verify that the JSON file is generated (it may be empty)
+    assert map_json is {}
+
+
+def test_generate_json_return_invalid_fields(tmpdir):
+    """Test the file information structure in generate_json()."""
+    # Create a temporary directory for testing
+    modpacks_directory = tmpdir.mkdir("modpacks")
+    base_api_url = "https://example.com/api/"
+
+    # Create subdirectories and files within the "modpacks" directory
+    modpack1 = modpacks_directory.mkdir("modpack1")
+    modpack1.mkdir("main_data").join("file1.txt").write("File 1 content")
+    modpack1.mkdir("client_data").join("file2.txt").write("File 2 content")
+    modpack1.mkdir("server_data").join("file3.txt").write("File 3 content")
+
+    modpack2 = modpacks_directory.mkdir("modpack2")
+    modpack2.mkdir("main_data").join("file4.txt").write("File 4 content")
+    modpack2.mkdir("client_data").join("file5.txt").write("File 5 content")
+    modpack2.mkdir("server_data").join("file6.txt").write("File 6 content")
+
+    # Execute the generate_json function with the specified JSON file path
+    map_json = generate_json(str(modpacks_directory), base_api_url)
+
+    assert map_json
+
+    # Define the expected structure of file information
+    expected_file_info_structure = {
+        "file_name",
+        "api_url",
+        "hash",
+        "install_on_client",
+        "install_on_server",
+        "dist_file_path",
+    }
+    # Check if the keys in the file_info match the expected structure
+    for _modpack, info in map_json.items():
+        for category_info in info.values():
+            for file_info in category_info:
+                assert set(file_info.keys()) == expected_file_info_structure
+
+
+# Add more test cases as needed
